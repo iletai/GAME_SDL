c2bad8572dd72c5137e4fe982776b715c3cb498d
iletai <tai.lequangtrong@gameloft.com>
2019-04-25 15:54:11 +0700

    ! Example load image by SDL

--- GAMESDL/GAMESDL/main.cpp
+++ GAMESDL/GAMESDL/main.cpp
@@ -1,8 +1,46 @@
-#include <Windows.h>
+#include <Windows.h>	 ]
+#include <string>
+
 #include <SDL.h>
+#include <SDL_image.h>
 #undef main
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+SDL_Surface* LoadImageByPath(std::string file_Path)
+{
+	SDL_Surface *load_Image		= NULL;
+	SDL_Surface *optimize_Image = NULL;
+
+	load_Image = IMG_Load(file_Path.c_str());
+	if (load_Image != NULL)
+	{
+		optimize_Image = SDL_DisplayFormat(load_Image);
+		SDL_Surface(load_Image);
+	}
+
+	return optimize_Image;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
+	SDL_Surface *screen;
+	SDL_Surface *image;
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
+		return 1;
+	
+	screen = SDL_SetVideoMode(1280, 1024, 32, SDL_SWSURFACE);
+	image = LoadImageByPath("JZBW2722.PNG");
+
+	SDL_BlitSurface(image, NULL, screen, NULL);
+
+	SDL_Flip(screen);
+	SDL_Delay(5000);
+	SDL_FreeSurface(image);
+	SDL_Quit();
+
 	return 0;
 }
